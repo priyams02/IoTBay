c3cbc0a023cddbfc6c11f8e00620daa9f9b3b60e
Brycekoh <[Brycekoh420@gmail.com]>
2025-03-27 20:29:44 +1100

    fixed package naming convention

--- src/main/classes/user.java
+++ src/main/classes/user.java
@@ -1,43 +1,61 @@
-package main.classes;
+package classes;
 
-import java.io.Serializable;
+import java.io.serializable;
 
-public class User implements Serializable {\
-private String name;
-    private String email;
+/*
+Base class for a person or user involved in IOTbay
+
+ */
+
+public class User implements serializable {
+    private static int totalUsers = 0;
+
+    public final int ID;
+    private String firstName;
+    private String lastName;
     private String password;
+    private String email;
 
-    public User() {
+    //    private Address address
+    private UserType type;
+    /*
+    Default constructor for a user
+    fn
+    ln
+    password
+    email
+    t type
+     */
+    public User(String firstName,String lastName,String password,String email,UserType type){
+        ID= totalUsers++;
 
+        firstName = firstName;
+        lastName = lastName;
+        password = password;
+        email = email;
+        type = type;
     }
 
-    public User(String name, String email, String password) {
-	this.name = name;
-        this.email = email;
-        this.password = password;
+    //    Getter and setter methods
+    public String getFirstname(){
+        return firstName;
     }
-
- public String getName() {
-        return name;
+    public String getLastName(){
+        return lastName;
     }
-    public String getEmail() {
-        return email;
-    }
-
     public String getPassword() {
         return password;
     }
-
- public void setName(String name) {
-        this.name = name;
+    public String getEmail(){
+        return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public UserType getType(){
+        return type;
     }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public enum UserType {
+        CUSTOMER,
+        STAFF
     }
-
 }
+
+
