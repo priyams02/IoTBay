2134ea700ffbca3dc6688f017e414ab0ed04daa2
DavidL138 <David.L.Lee-1@student.uts.edu.au>
2025-05-25 19:25:19 +1000

    Update PaymentServletsTest.java

--- src/test/java/uts/isd/model/DAO/PaymentServletsTest.java
+++ src/test/java/uts/isd/model/DAO/PaymentServletsTest.java
@@ -9,10 +9,12 @@ import org.mockito.Mockito;
 import uts.isd.Controller.CheckoutServlet;
 import uts.isd.model.DAO.DAO;
 import uts.isd.model.DAO.DBConnector;
+import uts.isd.model.Person.User;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class PaymentServletsTest {
@@ -34,7 +36,7 @@ public class PaymentServletsTest {
         CheckoutServlet servlet = new CheckoutServlet();
         servlet.doPost(request, response);
 
-        verify(session).setAttribute(eq("loggedInUser").getPaymentInformation(), any());
+        verify(session).setAttribute(eq("loggedInUser"), any());
     }
     
     @Test
@@ -52,8 +54,93 @@ public class PaymentServletsTest {
         CheckoutServlet servlet = new CheckoutServlet();
         servlet.doPost(request, response);
 
-        verify(session).setAttribute(eq("loggedInUser").getPaymentInformation(), eq(null));
+        verify(session).setAttribute(eq("loggedInUser"), eq(null));
     }
 
 
+    @Test
+    public void testValidEditPayments() throws ServletException, IOException, SQLException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+
+        when(request.getSession()).thenReturn(session);
+        User user = (User) session.getAttribute("loggedInUser");
+        when(request.getParameter("cardNo")).thenReturn("4501266849504");
+        when(request.getParameter("CVV")).thenReturn("123");
+        when(request.getParameter("CardHolder")).thenReturn("David Lee");
+
+        CheckoutServlet servlet = new CheckoutServlet();
+        servlet.doPost(request, response);
+
+        verify(session).setAttribute(eq("loggedInUser"), any());
+        User newUser = (User) session.getAttribute("loggedInUser");
+        assertNotEquals(user.getPaymentInfo().getCardNo(), newUser.getPaymentInfo().getCardNo());
+        assertNotEquals(user.getPaymentInfo().getCVV(), newUser.getPaymentInfo().getCVV());
+        assertNotEquals(user.getPaymentInfo().getCardHolder(), newUser.getPaymentInfo().getCardHolder());
+    }
+
+    @Test
+    public void testInvalidEditPayments() throws ServletException, IOException, SQLException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+
+        when(request.getSession()).thenReturn(session);
+        User user = (User) session.getAttribute("loggedInUser");
+        when(request.getParameter("cardNo")).thenReturn("4501266849504");
+        when(request.getParameter("CVV")).thenReturn("123");
+        when(request.getParameter("CardHolder")).thenReturn("David Lee");
+
+        CheckoutServlet servlet = new CheckoutServlet();
+        servlet.doPost(request, response);
+
+        verify(session).setAttribute(eq("loggedInUser"), any());
+        User newUser = (User) session.getAttribute("loggedInUser");
+        assertEquals(user.getPaymentInfo().getCardNo(), newUser.getPaymentInfo().getCardNo());
+        assertEquals(user.getPaymentInfo().getCVV(), newUser.getPaymentInfo().getCVV());
+        assertEquals(user.getPaymentInfo().getCardHolder(), newUser.getPaymentInfo().getCardHolder());
+    }
+
+    @Test
+    public void testValidDeletePayments() throws ServletException, IOException, SQLException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+
+        when(request.getSession()).thenReturn(session);
+        when(request.getParameter("cardNo")).thenReturn("4501266849504");
+        when(request.getParameter("CVV")).thenReturn("123");
+        when(request.getParameter("CardHolder")).thenReturn("David Lee");
+
+        CheckoutServlet servlet = new CheckoutServlet();
+        servlet.doPost(request, response);
+
+        verify(session).setAttribute(eq("loggedInUser"), any());
+        User newUser = (User) session.getAttribute("loggedInUser");
+        assertNull(newUser.getPaymentInfo().getCardNo());
+        assertNull(newUser.getPaymentInfo().getCVV());
+        assertNull(newUser.getPaymentInfo().getCardHolder());
+    }
+
+    @Test
+    public void testInvalidDeletePayments() throws ServletException, IOException, SQLException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+
+        when(request.getSession()).thenReturn(session);
+        when(request.getParameter("cardNo")).thenReturn("4501266849504");
+        when(request.getParameter("CVV")).thenReturn("123");
+        when(request.getParameter("CardHolder")).thenReturn("David Lee");
+
+        CheckoutServlet servlet = new CheckoutServlet();
+        servlet.doPost(request, response);
+
+        verify(session).setAttribute(eq("loggedInUser"), any());
+        User newUser = (User) session.getAttribute("loggedInUser");
+        assertNotNull(newUser.getPaymentInfo().getCardNo());
+        assertNotNull(newUser.getPaymentInfo().getCVV());
+        assertNotNull(newUser.getPaymentInfo().getCardHolder());
+    }
 }
